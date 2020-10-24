2d90deba4932523b631d9688469843c6642d04a9
CrabExcavator <yyclxx123@gmail.com>
2020-10-24 19:39:24 +0800

    sry

--- cmake-build-debug-system-coverage/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/weihan/CLionProjects/player
-# Build directory: /Users/weihan/CLionProjects/player/cmake-build-debug-system-coverage
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
